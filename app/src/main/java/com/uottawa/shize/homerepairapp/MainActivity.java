14a13de63b987fd595904a9fa2cbaafc457922e4
ShizeMation <1732145@gmail.com>
2018-10-23 18:43:37 -0400

    Created start screen UI

--- app/src/main/java/com/uottawa/shize/homerepairapp/MainActivity.java
+++ app/src/main/java/com/uottawa/shize/homerepairapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.uottawa.shize.homerepairapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickLogin(View view) {
+
+    }
+
+    public void onClickSignup(View view) {
+
     }
 }
