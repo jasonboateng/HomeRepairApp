e20246eb29ef7260da435311d0ee161f058ceb74
ShizeMation <1732145@gmail.com>
2018-10-24 14:39:42 -0400

    Added Firebase support and created login page

--- app/src/main/java/com/uottawa/shize/homerepairapp/LoginActivity.java
+++ app/src/main/java/com/uottawa/shize/homerepairapp/LoginActivity.java
@@ -5,20 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
-    public void onClickChooseLogin(View view) {
-        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+    public void onClickBack(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
-    }
-
-    public void onClickChooseSignup(View view) {
-
     }
 }
